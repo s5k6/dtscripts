741b8cf50aea77feca685d20273bf7a7d1fcd364
Stefan Klinger <git@stefan-klinger.de>
2019-06-09 12:26:04 +0200

    applyStyleOnImport: DT can disable base curve

--- applyStyleOnImport.lua
+++ applyStyleOnImport.lua
@@ -1,6 +1,11 @@
 
---[[ I use this script to disable "base curve" on imported images.  It
-    should be able to auto-apply any style on import.
+--[[ This script should be able to auto-apply any style on import.
+
+    I've built this script to disable "base curve" on imported images.
+    This is not necessary for the current development branch any more,
+    since it allows to disable the automatic application of "base
+    curve" in "Settings → core options → quality".  (Version
+    2.7.0+1375~g6f0010465)
 
     To use, import the style `minimal.dtstyle` (switches off the
     basecurve).  Then "require" this script from `luarc` by something
@@ -9,8 +14,6 @@
         require('dtscripts/applyStyleOnImport').use('minimal')
 
     but read `README` to get the path right.  Then restart darktable.
-
-    This is a workaround until basecurve is disabled by default.
 ]]
 
 
@@ -30,7 +33,7 @@ local m = {} -- module export
 
 m.use = function(styleName)
 
-    -- find style of this name 
+    -- find style of this name
     local style = nil
     for k, v in ipairs(dt.styles) do
         if v.name == styleName then
